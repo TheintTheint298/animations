fde9f7afd196db24922944bbfda8d18743f827f8
TheintTheint298 <theinttheintsoehtet@gmail.com>
2026-09-04 21:16:44 +0630

    feat: animating disappearances/removal

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react';
+import { useState } from "react";
+import { AmimatePresence } from "framer-motion";
 
-import NewChallenge from './NewChallenge.jsx';
+import NewChallenge from "./NewChallenge.jsx";
 
 export default function Header() {
   const [isCreatingNewChallenge, setIsCreatingNewChallenge] = useState();
@@ -15,7 +16,9 @@ export default function Header() {
 
   return (
     <>
-      {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+      <AmimatePresence>
+        {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+      </AmimatePresence>
 
       <header id="main-header">
         <h1>Your Challenges</h1>
